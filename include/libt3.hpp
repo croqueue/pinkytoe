a4be62bc8a75878b717dcd0f173ac52d7494c512
Danielle Thompson <danielle.thompson.9002@gmail.com>
2025-02-28 17:18:17 -0600

    fixed bug in libt3::TicTacToeBoard::TicTacToeBoard(CellOwner)

--- include/libt3.hpp
+++ include/libt3.hpp
@@ -1,6 +1,8 @@
 #ifndef ISLANDU_LIBT3_HPP_
 #define ISLANDU_LIBT3_HPP_
 
+#include <stdexcept>
+
 namespace libt3 {
 
 /// @brief 
@@ -9,6 +11,7 @@ enum class CellOwner : int {
     None    = 0,
     PlayerX = 1
 };
+
 
 /// @brief 
 enum class MoveResult : int {
@@ -19,8 +22,14 @@ enum class MoveResult : int {
 
 /// @brief TODO:
 class TicTacToeBoard {
+    /// @brief 
     CellOwner grid_[3][3];
+    /// @brief 
     CellOwner next_move_;
+    /// @brief 
+    int row_bals_[3];
+    int col_bals_[3];
+    int diag_bals_[2];
 
     /// @brief TODO:
     /// @param row 
@@ -46,8 +55,13 @@ class TicTacToeBoard {
 public:
     /// @brief TODO:
     /// @param init_player 
-    inline constexpr TicTacToeBoard(CellOwner init_player) noexcept : grid_{}, next_move_(init_player)
+    inline constexpr TicTacToeBoard(CellOwner init_player) noexcept
+        : grid_{}, next_move_(init_player),
+          row_bals_{}, col_bals_{}, diag_bals_{}
     {
+        if (init_player == CellOwner::None) {
+            throw std::invalid_argument("Starting player required");
+        }
     }
 
     /// @brief TODO:
