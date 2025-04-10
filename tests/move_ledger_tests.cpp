384ca32bf7b206e49d6cf865786d64bd2fe9ebcb
Danielle Thompson <danielle.thompson.9002@gmail.com>
2025-04-10 14:02:02 -0500

    started on pinkytoe::impl::MoveLedger tests

--- tests/move_ledger_tests.cpp
+++ tests/move_ledger_tests.cpp
@@ -2,6 +2,11 @@
 #include "pinkytoe/enums.h"
 #include "move_ledger.hpp"
 
+using pinkytoe::Player;
+using pinkytoe::impl::MoveLedger;
+
 TEST(MoveLedgerTests, constructor_test) {
-  ASSERT_EQ("hello", "Hello");
+  MoveLedger ledger(Player::X);
+  ASSERT_EQ(ledger.first_player(), Player::X);
+  ASSERT_EQ(ledger.move_count(), 0);
 }
