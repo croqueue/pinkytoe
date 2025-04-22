f0fd773725574e738a2e62b530b7bec7c3d402a0
Danielle Thompson <danielle.thompson.9002@gmail.com>
2025-04-22 18:38:43 -0500

    basic_term example app started (sloppy copy...not even sure if it builds)

---
+++ examples/basic_term/src/enums.h
@@ -0,0 +1,45 @@
+#include <cstdint>
+
+namespace pinkytoe::examples::basic_term {
+
+enum class ForegroundColor : std::uint8_t
+{
+  Black = 30,
+  Red = 31,
+  Green = 32,
+  Yellow = 33,
+  Blue = 34,
+  Magenta = 35,
+  Cyan = 36,
+  White = 37,
+  BrightBlack = 90,
+  BrightRed = 91,
+  BrightGreen = 92,
+  BrightYellow = 93,
+  BrightBlue = 94,
+  BrightMagenta = 95,
+  BrightCyan = 96,
+  BrightWhite = 97
+};
+
+enum class BackgroundColor : std::uint8_t
+{
+  Black = 40,
+  Red = 41,
+  Green = 42,
+  Yellow = 43,
+  Blue = 44,
+  Magenta = 45,
+  Cyan = 46,
+  White = 47,
+  BrightBlack = 100,
+  BrightRed = 101,
+  BrightGreen = 102,
+  BrightYellow = 103,
+  BrightBlue = 104,
+  BrightMagenta = 105,
+  BrightCyan = 106,
+  BrightWhite = 107
+};
+
+} // namespace pinkytoe::examples::basic_term
---
+++ examples/basic_term/src/main.cpp
@@ -0,0 +1,112 @@
+#include "enums.h"
+#include "pinkytoe/omni.h"
+#include <cstdio>
+
+using pinkytoe::BasicToe;
+using pinkytoe::Column;
+using pinkytoe::MoveResult;
+using pinkytoe::Player;
+using pinkytoe::Row;
+
+namespace pinkytoe::examples::basic_term {
+
+struct GameConfig final
+{
+  char x_repr, o_repr;
+  ForegroundColor foreground_color;
+  BackgroundColor background_color;
+
+  inline constexpr GameConfig() noexcept
+    : x_repr('X')
+    , o_repr('O')
+    , foreground_color(ForegroundColor::White)
+    , background_color(BackgroundColor::BrightBlack)
+  {
+  }
+};
+
+class GameContext final
+{
+  BasicToe game_;
+  const GameConfig config_;
+  Player frame_buf_[3][3];
+
+public:
+  /// @brief
+  /// @param player1
+  inline GameContext(Player player1) noexcept
+    : game_(player1)
+    , config_{}
+    , frame_buf_{}
+  {
+  }
+
+  /// @brief
+  /// @param player1
+  /// @param config
+  inline GameContext(Player player1, const GameConfig& config) noexcept
+    : game_(player1)
+    , config_(config)
+    , frame_buf_{}
+  {
+  }
+
+  inline constexpr MoveResult play_next(Row row, Column column) noexcept
+  {
+    return this->game_.play_next(row, column);
+  }
+
+  inline void draw_state() noexcept
+  {
+    this->game_.read_board(this->frame_buf_);
+    auto fg = static_cast<unsigned int>(this->config_.foreground_color);
+    auto bg = static_cast<unsigned int>(this->config_.background_color);
+
+    for (auto r = 0; r < 3; ++r) {
+      for (auto c = 0; c < 3; ++c) {
+        char owner;
+
+        switch (this->frame_buf_[r][c]) {
+          case Player::X:
+            owner = this->config_.x_repr;
+            break;
+          case Player::O:
+            owner = this->config_.o_repr;
+            break;
+          default:
+            owner = ' ';
+            break;
+        }
+
+        if ((r + c) & 1) {
+          std::fprintf(stdout, "\033[%u;%um%c[0m", fg, bg, owner);
+        } else {
+          std::fprintf(stdout, "\033[%um%c[0m", fg, owner);
+        }
+      }
+
+      std::fputc('\n', stdout);
+    }
+  }
+};
+
+} // namespace pinkytoe::examples::basic_term
+
+int
+main(void)
+{
+  using pinkytoe::examples::basic_term::BackgroundColor;
+  using pinkytoe::examples::basic_term::ForegroundColor;
+  using pinkytoe::examples::basic_term::GameConfig;
+  using pinkytoe::examples::basic_term::GameContext;
+
+  GameConfig config;
+  GameContext context(Player::X, config);
+
+  context.play_next(Row::Middle, Column::Middle);
+  context.draw_state();
+
+  // RAII w/ GameContext upon processing prerequisite CL options and user input
+
+  return 0;
+}
